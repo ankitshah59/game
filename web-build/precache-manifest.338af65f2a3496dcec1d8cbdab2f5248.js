8bafe25c6babe5971a6baf827c9d19eadbab9361
Ankit <ankit.shah-2@selu.edu>
2020-11-09 23:15:19 -0700

    web build added

---
+++ web-build/precache-manifest.338af65f2a3496dcec1d8cbdab2f5248.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "112db0d6c1d3f9c4c3fe",
+    "url": "/static/js/app.4570a534.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "a9ced4e7ec5dc27f9046",
+    "url": "/static/js/2.b5fa992b.chunk.js"
+  },
+  {
+    "revision": "864b5a2a12e395a0aba167209a78df95",
+    "url": "/static/media/rock.864b5a2a.png"
+  },
+  {
+    "revision": "48d238a67d2b41996cd45194a93b7b10",
+    "url": "/static/media/scissors.48d238a6.png"
+  },
+  {
+    "revision": "cffb3100c0baea623b0353f9e933ef62",
+    "url": "/static/media/paper.cffb3100.png"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "7edaad284c66ffd18479ee414945bb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72527abc23c6c5ce3cfeb31046371bbe",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
